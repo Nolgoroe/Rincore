8f9e8fc9010a141deaee958a625d98bcd421c57a
Avishy Menashe <avishy.menashe@gmail.com>
2023-10-19 18:51:48 +0300

    fixes

--- Assets/Scripts/Custom UI/Buttons/BasicCustomButton.cs
+++ Assets/Scripts/Custom UI/Buttons/BasicCustomButton.cs
@@ -23,4 +23,5 @@ public class BasicCustomButton : CustomButtonParent
     {
         base.SetMyElement(texts, sprites);
     }
+
 }
--- Assets/Scripts/Custom UI/CustomButtonParent.cs
+++ Assets/Scripts/Custom UI/CustomButtonParent.cs
@@ -21,8 +21,23 @@ public abstract class CustomButtonParent : BasicUIElement, IPointerDownHandler
 
 
     [SerializeField] protected bool isUseOnce;
+    [SerializeField] protected bool effectDisplayOnClick = true;
 
 
+    private float timeToShrink = 1;
+    private float timeToGrow = 1;
+    private float timeToRevertClick = 1;
+    private float shrinkBySize = 0.8f;
+
+    private void Start()
+    {
+        effectDisplayOnClick = true;
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
     private void OnMouseDown()
     {
         if (isInteractable && 
@@ -33,6 +48,11 @@ public abstract class CustomButtonParent : BasicUIElement, IPointerDownHandler
             // play click sound
             //SoundManager.instance.CallPlaySound(sounds.ButtonClick);
             OnClickButton();
+
+            if (effectDisplayOnClick && isActiveAndEnabled)
+            {
+                StartCoroutine(AnimateClick());
+            }
         }
     }
 
@@ -49,6 +69,30 @@ public abstract class CustomButtonParent : BasicUIElement, IPointerDownHandler
         {
             // play click sound
             OnClickButton();
+
+            if (effectDisplayOnClick && isActiveAndEnabled)
+            {
+                StartCoroutine(AnimateClick());
+            }
         }
+    }
+
+    private IEnumerator AnimateClick()
+    {
+        // these numbers were tested in the scene - these are the decided values.
+        // if we wish to change these values again, make these values serializable again.
+        // we did this so we don't need to change every single button in the scene.
+        timeToShrink = 0.075f;
+        timeToGrow = 0.075f;
+        timeToRevertClick = 0.1f;
+        shrinkBySize = 0.95f;
+
+        Vector3 currentSize = transform.localScale;
+        Vector3 newSize = currentSize * shrinkBySize;
+
+        LeanTween.scale(gameObject, newSize, timeToShrink);
+        yield return new WaitForSeconds(timeToRevertClick);
+        LeanTween.scale(gameObject, currentSize, timeToGrow);
+
     }
 }
